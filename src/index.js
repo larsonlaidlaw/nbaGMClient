f019dc06b9f0df74859526ae4208e9b9942636ff
github username <github email address>
2018-04-06 15:35:33 -0400

    edited composeEnhancers logic

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducers/reducer'
 
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
 
 const rootReducer = combineReducers({
   reducer: reducer,
